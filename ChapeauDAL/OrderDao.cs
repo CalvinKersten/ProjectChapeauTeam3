8dc69b8d063f4557f036eb01eb6ae675e723c308
CalvinKersten <113361043+CalvinKersten@users.noreply.github.com>
2023-05-23 15:10:43 +0200

    Project Chapeau Base
    base DAL, Model and Service layers for Project Chapeau

---
+++ ChapeauDAL/OrderDao.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using ChapeauModel;
+
+
+namespace ChapeauDAL
+{
+    public class OrderDao : BaseDao
+    {
+        public List<Order> GetAllOrders()
+        {
+            string query = "SELECT OrderID, Table_Num, Total_Price FROM Order";
+            SqlParameter[] sqlParameters = new SqlParameter[0];
+            return ReadTables(ExecuteSelectQuery(query, sqlParameters));
+        }
+
+        private List<Order> ReadTables(DataTable dataTable)
+        {
+            List<Order> orders = new List<Order>();
+
+            foreach (DataRow dr in dataTable.Rows)
+            {
+                Order order = new Order()
+                {
+                    OrderID = (int)dr["OrderID"],
+                    Table_Num = (int)dr["Table_Num"],
+                    Total_Price = (float)dr["Total_Price"],
+                };
+                orders.Add(order);
+            }
+            return orders;
+        }
+
+    }
+}
+
