980073053ab22eae21c004bda57385913fa52d56
CalvinKersten <113361043+CalvinKersten@users.noreply.github.com>
2023-06-09 22:26:11 +0200

    UI

---
+++ ChapeauUI/ButtonStyle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChapeauUI
+{
+    class ButtonStyle : Button
+    {
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+            GraphicsPath path = new GraphicsPath();
+            int arcRadius = 25; // Adjust the radius to control the amount of curvature
+
+            int width = Width;
+            int height = Height;
+
+            path.AddArc(0, 0, arcRadius, arcRadius, 180, 90);
+            path.AddArc(width - arcRadius, 0, arcRadius, arcRadius, 270, 90);
+            path.AddArc(width - arcRadius, height - arcRadius, arcRadius, arcRadius, 0, 90);
+            path.AddArc(0, height - arcRadius, arcRadius, arcRadius, 90, 90);
+            path.CloseFigure();
+
+            Region = new Region(path);
+            base.OnPaint(pevent);
+        }
+    }
+    class LabelStyle : Label
+    {
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+            GraphicsPath path = new GraphicsPath();
+            int arcRadius = 25; // Adjust the radius to control the amount of curvature
+
+            int width = Width;
+            int height = Height;
+
+            path.AddArc(0, 0, arcRadius, arcRadius, 180, 90);
+            path.AddArc(width - arcRadius, 0, arcRadius, arcRadius, 270, 90);
+            path.AddArc(width - arcRadius, height - arcRadius, arcRadius, arcRadius, 0, 90);
+            path.AddArc(0, height - arcRadius, arcRadius, arcRadius, 90, 90);
+            path.CloseFigure();
+
+            Region = new Region(path);
+            base.OnPaint(pevent);
+        }
+    }
+}
+
--- ChapeauUI/Form1.Designer.cs
+++ ChapeauUI/Form1.Designer.cs
@@ -29,13 +29,18 @@
         private void InitializeComponent()
         {
             this.pnlOrderOverview = new System.Windows.Forms.Panel();
+            this.LunchNavButton = new ChapeauUI.ButtonStyle();
             this.pnlOrderHome = new System.Windows.Forms.Panel();
             this.pnlOrderNav = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.DrinksNavButton = new ChapeauUI.ButtonStyle();
+            this.DinnerNavButton = new ChapeauUI.ButtonStyle();
+            this.labelStyle1 = new ChapeauUI.LabelStyle();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.ColumnHeader1 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader3 = new System.Windows.Forms.ColumnHeader();
             this.pnlOrderOverview.SuspendLayout();
             this.pnlOrderHome.SuspendLayout();
             this.pnlOrderNav.SuspendLayout();
@@ -44,65 +49,92 @@
             // 
             // pnlOrderOverview
             // 
-            this.pnlOrderOverview.Controls.Add(this.pnlOrderHome);
-            this.pnlOrderOverview.Controls.Add(this.pnlOrderNav);
+            this.pnlOrderOverview.Controls.Add(this.listView1);
             this.pnlOrderOverview.Location = new System.Drawing.Point(-3, 1);
             this.pnlOrderOverview.Name = "pnlOrderOverview";
             this.pnlOrderOverview.Size = new System.Drawing.Size(401, 695);
             this.pnlOrderOverview.TabIndex = 0;
             // 
+            // LunchNavButton
+            // 
+            this.LunchNavButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.LunchNavButton.FlatAppearance.BorderSize = 0;
+            this.LunchNavButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.LunchNavButton.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LunchNavButton.Location = new System.Drawing.Point(11, 7);
+            this.LunchNavButton.Name = "LunchNavButton";
+            this.LunchNavButton.Size = new System.Drawing.Size(98, 56);
+            this.LunchNavButton.TabIndex = 0;
+            this.LunchNavButton.Text = "Lunch";
+            this.LunchNavButton.UseVisualStyleBackColor = false;
+            // 
             // pnlOrderHome
             // 
+            this.pnlOrderHome.Controls.Add(this.pnlOrderNav);
+            this.pnlOrderHome.Controls.Add(this.labelStyle1);
             this.pnlOrderHome.Controls.Add(this.pictureBox1);
             this.pnlOrderHome.Controls.Add(this.label1);
-            this.pnlOrderHome.Location = new System.Drawing.Point(3, 0);
+            this.pnlOrderHome.Location = new System.Drawing.Point(0, 0);
             this.pnlOrderHome.Name = "pnlOrderHome";
-            this.pnlOrderHome.Size = new System.Drawing.Size(398, 73);
+            this.pnlOrderHome.Size = new System.Drawing.Size(395, 149);
             this.pnlOrderHome.TabIndex = 2;
             // 
             // pnlOrderNav
             // 
-            this.pnlOrderNav.Controls.Add(this.button1);
-            this.pnlOrderNav.Controls.Add(this.button2);
-            this.pnlOrderNav.Controls.Add(this.button3);
-            this.pnlOrderNav.Location = new System.Drawing.Point(3, 74);
+            this.pnlOrderNav.Controls.Add(this.DrinksNavButton);
+            this.pnlOrderNav.Controls.Add(this.DinnerNavButton);
+            this.pnlOrderNav.Controls.Add(this.LunchNavButton);
+            this.pnlOrderNav.Location = new System.Drawing.Point(0, 79);
             this.pnlOrderNav.Name = "pnlOrderNav";
             this.pnlOrderNav.Size = new System.Drawing.Size(398, 70);
             this.pnlOrderNav.TabIndex = 2;
             // 
-            // button1
+            // DrinksNavButton
             // 
-            this.button1.Location = new System.Drawing.Point(15, 2);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(118, 68);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.DrinksNavButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.DrinksNavButton.FlatAppearance.BorderSize = 0;
+            this.DrinksNavButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.DrinksNavButton.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.DrinksNavButton.Location = new System.Drawing.Point(285, 7);
+            this.DrinksNavButton.Name = "DrinksNavButton";
+            this.DrinksNavButton.Size = new System.Drawing.Size(98, 56);
+            this.DrinksNavButton.TabIndex = 2;
+            this.DrinksNavButton.Text = "Drinks";
+            this.DrinksNavButton.UseVisualStyleBackColor = false;
             // 
-            // button2
+            // DinnerNavButton
             // 
-            this.button2.Location = new System.Drawing.Point(139, 2);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(118, 68);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.DinnerNavButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.DinnerNavButton.FlatAppearance.BorderSize = 0;
+            this.DinnerNavButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.DinnerNavButton.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.DinnerNavButton.Location = new System.Drawing.Point(146, 7);
+            this.DinnerNavButton.Name = "DinnerNavButton";
+            this.DinnerNavButton.Size = new System.Drawing.Size(98, 56);
+            this.DinnerNavButton.TabIndex = 1;
+            this.DinnerNavButton.Text = "Dinner";
+            this.DinnerNavButton.UseVisualStyleBackColor = false;
             // 
-            // button3
+            // labelStyle1
             // 
-            this.button3.Location = new System.Drawing.Point(264, 2);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(118, 68);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.labelStyle1.AutoSize = true;
+            this.labelStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.labelStyle1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.labelStyle1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelStyle1.Location = new System.Drawing.Point(288, 20);
+            this.labelStyle1.Name = "labelStyle1";
+            this.labelStyle1.Padding = new System.Windows.Forms.Padding(5);
+            this.labelStyle1.Size = new System.Drawing.Size(89, 42);
+            this.labelStyle1.TabIndex = 0;
+            this.labelStyle1.Text = "Calvin";
+            this.labelStyle1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::ChapeauUI.Properties.Resources.Logo;
-            this.pictureBox1.Location = new System.Drawing.Point(3, 0);
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(150, 73);
+            this.pictureBox1.Size = new System.Drawing.Size(137, 80);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -110,12 +142,40 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(159, 20);
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Verdana Pro Light", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(140, 24);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(102, 37);
+            this.label1.Size = new System.Drawing.Size(119, 32);
             this.label1.TabIndex = 1;
             this.label1.Text = "Table #";
+            // 
+            // listView1
+            // 
+            this.listView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ColumnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.Location = new System.Drawing.Point(14, 166);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(372, 362);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // ColumnHeader1
+            // 
+            this.ColumnHeader1.Text = "no.";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "name";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "price";
+            this.columnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // Form1
             // 
@@ -123,6 +183,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.ClientSize = new System.Drawing.Size(398, 697);
+            this.Controls.Add(this.pnlOrderHome);
             this.Controls.Add(this.pnlOrderOverview);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -140,10 +201,15 @@
         private Panel pnlOrderOverview;
         private PictureBox pictureBox1;
         private Label label1;
-        private Button button1;
-        private Button button2;
-        private Button button3;
         private Panel pnlOrderNav;
         private Panel pnlOrderHome;
+        private ButtonStyle LunchNavButton;
+        private ButtonStyle DrinksNavButton;
+        private ButtonStyle DinnerNavButton;
+        private LabelStyle labelStyle1;
+        private ListView listView1;
+        private ColumnHeader ColumnHeader1;
+        private ColumnHeader columnHeader2;
+        private ColumnHeader columnHeader3;
     }
 }
