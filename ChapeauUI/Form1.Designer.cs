2ec4c41575e7a26ee59683ebffcfd9eb2a1dd257
CalvinKersten <113361043+CalvinKersten@users.noreply.github.com>
2023-06-09 18:58:08 +0200

    Added Nav Bar

--- ChapeauUI/Form1.Designer.cs
+++ ChapeauUI/Form1.Designer.cs
@@ -29,26 +29,73 @@
         private void InitializeComponent()
         {
             this.pnlOrderOverview = new System.Windows.Forms.Panel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.pnlOrderHome = new System.Windows.Forms.Panel();
+            this.pnlOrderNav = new System.Windows.Forms.Panel();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.pnlOrderNav = new System.Windows.Forms.Panel();
-            this.pnlOrderHome = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.pnlOrderOverview.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.pnlOrderNav.SuspendLayout();
             this.pnlOrderHome.SuspendLayout();
+            this.pnlOrderNav.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pnlOrderOverview
             // 
             this.pnlOrderOverview.Controls.Add(this.pnlOrderHome);
+            this.pnlOrderOverview.Controls.Add(this.pnlOrderNav);
             this.pnlOrderOverview.Location = new System.Drawing.Point(-3, 1);
             this.pnlOrderOverview.Name = "pnlOrderOverview";
             this.pnlOrderOverview.Size = new System.Drawing.Size(401, 695);
             this.pnlOrderOverview.TabIndex = 0;
+            // 
+            // pnlOrderHome
+            // 
+            this.pnlOrderHome.Controls.Add(this.pictureBox1);
+            this.pnlOrderHome.Controls.Add(this.label1);
+            this.pnlOrderHome.Location = new System.Drawing.Point(3, 0);
+            this.pnlOrderHome.Name = "pnlOrderHome";
+            this.pnlOrderHome.Size = new System.Drawing.Size(398, 73);
+            this.pnlOrderHome.TabIndex = 2;
+            // 
+            // pnlOrderNav
+            // 
+            this.pnlOrderNav.Controls.Add(this.button1);
+            this.pnlOrderNav.Controls.Add(this.button2);
+            this.pnlOrderNav.Controls.Add(this.button3);
+            this.pnlOrderNav.Location = new System.Drawing.Point(3, 74);
+            this.pnlOrderNav.Name = "pnlOrderNav";
+            this.pnlOrderNav.Size = new System.Drawing.Size(398, 70);
+            this.pnlOrderNav.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 68);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(139, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 68);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(264, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(118, 68);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // pictureBox1
             // 
@@ -70,53 +117,6 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "Table #";
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(0, 0);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(118, 68);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(0, 0);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(118, 68);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(0, 0);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(118, 68);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // pnlOrderNav
-            // 
-            this.pnlOrderNav.Controls.Add(this.button1);
-            this.pnlOrderNav.Controls.Add(this.button2);
-            this.pnlOrderNav.Controls.Add(this.button3);
-            this.pnlOrderNav.Location = new System.Drawing.Point(3, 70);
-            this.pnlOrderNav.Name = "pnlOrderNav";
-            this.pnlOrderNav.Size = new System.Drawing.Size(401, 70);
-            this.pnlOrderNav.TabIndex = 2;
-            // 
-            // pnlOrderHome
-            // 
-            this.pnlOrderHome.Controls.Add(this.pnlOrderNav);
-            this.pnlOrderHome.Controls.Add(this.pictureBox1);
-            this.pnlOrderHome.Controls.Add(this.label1);
-            this.pnlOrderHome.Location = new System.Drawing.Point(15, 140);
-            this.pnlOrderHome.Name = "pnlOrderHome";
-            this.pnlOrderHome.Size = new System.Drawing.Size(401, 73);
-            this.pnlOrderHome.TabIndex = 2;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
@@ -127,10 +127,10 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.pnlOrderOverview.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.pnlOrderNav.ResumeLayout(false);
             this.pnlOrderHome.ResumeLayout(false);
             this.pnlOrderHome.PerformLayout();
+            this.pnlOrderNav.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
