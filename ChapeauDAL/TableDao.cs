b12fe5f14124b6a9ae9c2b2c137597feecb1a8e4
CalvinKersten <113361043+CalvinKersten@users.noreply.github.com>
2023-06-13 08:59:00 +0200

    Table Numbers show in Table order overview

--- ChapeauDAL/TableDao.cs
+++ ChapeauDAL/TableDao.cs
@@ -14,16 +14,30 @@ namespace ChapeauDAL
     {
         public List<Table> GetAllTables()
         {
-            string query = "SELECT TableID, Table_Num, Capacity FROM Table";
+            string query = "SELECT TableID, Table_Num, Capacity FROM [Table]";
             SqlParameter[] sqlParameters = new SqlParameter[0];
             return ReadTables(ExecuteSelectQuery(query, sqlParameters));
         }
 
         public List<Table> GetAllTableNumbers()
         {
-            string query = "SELECT Table_Num FROM Table";
+            string query = "SELECT Table_Num FROM [Table]";
             SqlParameter[] sqlParameters = new SqlParameter[0];
-            return ReadTables(ExecuteSelectQuery(query, sqlParameters));
+            return ReadTableNumber(ExecuteSelectQuery(query, sqlParameters));
+        }
+        private List<Table> ReadTableNumber(DataTable dataTable)
+        {
+            List<Table> tableNumber = new List<Table>();
+
+            foreach (DataRow dr in dataTable.Rows)
+            {
+                Table table = new Table()
+                {
+                    Table_Num = (int)dr["Table_Num"]
+                };
+                tableNumber.Add(table);
+            }
+            return tableNumber;
         }
         private List<Table> ReadTables(DataTable dataTable)
         {
